f150fc420da66e39d0ebda128dca2d8790eaeb2e
phuong <phuongdenpro@gmail.com>
2022-09-26 13:57:14 +0700

    fix

--- src/main/java/com/example/springbootdatajpa/repository/NhanVienRepository.java
+++ src/main/java/com/example/springbootdatajpa/repository/NhanVienRepository.java
@@ -4,10 +4,14 @@ import com.example.springbootdatajpa.entity.NhanVien;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+
+import java.util.Map;
 import java.util.List;
 
 @Repository
-public interface NhanVienRepository extends JpaRepository<NhanVien,Long> {
-    List<NhanVien> findEmployeeByLuong(long luong);
+public interface NhanVienRepository extends JpaRepository<NhanVien,String> {
+
 }
